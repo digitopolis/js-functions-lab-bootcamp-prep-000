c801a82b716f5c40335b2c5bef5adc1da78dba21
Matt Readout <matt.readout@gmail.com>
2018-08-27 18:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // write your code below!
-function happyHolidays () {
+function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo (name) {
-  return `Happy holidays, ${name}`
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown
